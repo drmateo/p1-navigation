b2edc8ebdf466a5ad39a73ea1312568310912142
Carlos M. Mateo <cmmateo86@gmail.com>
2020-05-16 02:28:59 +0200

    feat: double dqn model/agent add and functional

--- train.py
+++ train.py
@@ -1,3 +1,17 @@
+# MIT License
+#
+# Copyright (c) 2020 Carlos M. Mateo
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
 import random
 import torch
 import numpy as np
@@ -12,7 +26,47 @@ from dqn_agent import Agent
 # use ggplot style for more sophisticated visuals
 plt.style.use('ggplot')
 
-def dqn(env, agent, n_episodes=2000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+import threading, queue
+import time
+
+exitFlag = 0
+
+class Monitor ():
+    def __init__(self, n_episodes=2000):
+        self.ax = dict()
+        self.val = dict()
+
+        self.fig = plt.figure(figsize=(8, 12))
+        self.ax['top'] = self.fig.add_subplot(211)
+        self.ax['top'].set_ylabel('Score')
+        # self.ax['top'].set_xlabel('Episode #')
+        self.ax['down'] = self.fig.add_subplot(212)
+        self.ax['down'].set_ylabel('eps')
+        #self.ax['down'].set_yscale('log')
+        self.ax['down'].set_xlabel('Episode #')
+
+        self.ax['top'].set_xlim(0, n_episodes + 20)
+        self.ax['top'].set_ylim(-5, +40)
+        self.val['score'], = self.ax['top'].plot([], [], 'b-', label='curr', alpha=0.3)
+        self.val['score_mean'], = self.ax['top'].plot([], [], 'r-', label='mean')
+        self.val['score_min'], = self.ax['top'].plot([], [], '-', color='burlywood', label='min')
+        self.val['score_max'], = self.ax['top'].plot([], [], linestyle='-', color='green', label='max')
+
+        self.ax['top'].legend()
+
+        self.ax['down'].set_xlim(0, n_episodes + 20)
+        self.ax['down'].set_ylim(0, 1)
+        self.val['eps'], = self.ax['down'].plot([], [], 'b-')
+
+    def draw(self, val):
+        for key in self.val:
+            self.val[key].set_xdata(np.arange(len(val[key])))
+            self.val[key].set_ydata(val[key])
+
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
+def dqn(env, agent, n_episodes=2000, eps_start=1.0, eps_end=0.01, eps_decay=0.995, window_size=500, do_visualization=False):
     """Deep Q-Learning.
     
     Params
@@ -25,38 +79,22 @@ def dqn(env, agent, n_episodes=2000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
         eps_end (float): minimum value of epsilon
         eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
     """
+    plt.ion()
+    monitor = Monitor(n_episodes)
+    track = {'score' : [], 
+             'score_mean' : [],
+             'score_min' : [],
+             'score_max' : [],
+             'eps' : [],
+             'score_window' : deque(maxlen=window_size)}
 
     # get the default brain
     brain_name = env.brain_names[0]
     brain = env.brains[brain_name]
 
-    fig = plt.figure(figsize=(8, 12))
-    axes1 = fig.add_subplot(211)
-    axes1.set_ylabel('Score')
-    # axes1.set_xlabel('Episode #')
-    axes2 = fig.add_subplot(212)
-    axes2.set_ylabel('eps')
-    #axes2.set_yscale('log')
-    axes2.set_xlabel('Episode #')
-
-    axes1.set_xlim(0, n_episodes + 20)
-    axes1.set_ylim(-5, +40)
-    line1, = axes1.plot([], [], 'b-')
-    line2, = axes1.plot([], [], 'r-')
-
-    axes2.set_xlim(0, n_episodes + 20)
-    axes2.set_ylim(0, 1)
-    line3, = axes2.plot([], [], 'b-')
-
-    scores = []                        # list containing scores from each episode
-    scores_mean = []
-    epss = []
-    scores_window = deque(maxlen=100)  # last 100 scores
     max_score = 0
-    
     eps = eps_start                    # initialize epsilon
     for i_episode in range(1, n_episodes+1):
-
         env_info = env.reset(train_mode=True)[brain_name] # reset the environment
         action_size = brain.vector_action_space_size       # number of actions
         state = env_info.vector_observations[0]            # get the current state
@@ -74,38 +112,34 @@ def dqn(env, agent, n_episodes=2000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
             if done:                                       # exit loop if episode finished
                 break
 
-        scores_window.append(score)                 # save most recent score
-        scores.append(score)                        # save most recent score
-        scores_mean.append(np.mean(scores_window))  # save most recent mean score
-        epss.append(eps)                            # save eps
+        track['score_window'].append(score)                 # save most recent score
+        track['score'].append(score)                        # save most recent score
+        track['score_mean'].append(np.mean(track['score_window']))  # save most recent mean score
+        track['score_min'].append(np.min(track['score_window']))  # save most recent mean score
+        track['score_max'].append(np.max(track['score_window']))  # save most recent mean score
+        track['eps'].append(eps)                            # save eps
 
-        line1.set_xdata(np.arange(len(scores)))
-        line1.set_ydata(scores)
-        
-        line2.set_xdata(np.arange(len(scores_mean)))
-        line2.set_ydata(scores_mean)
+        if do_visualization: monitor.draw(track)
 
-        line3.set_xdata(np.arange(len(epss)))
-        line3.set_ydata(epss)
-
-        fig.canvas.draw()
-
-        eps = max(eps_end, eps_decay*eps) # decrease epsilon
-        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, scores_mean[-1]), end="")
+        print('\rEpisode {}\tAverage Score: {:.2f}\tMin Score: {:.2f}\tMax Score: {:.2f}'.format(i_episode, track['score_mean'][-1], track['score_min'][-1], track['score_max'][-1]), end="")
         if i_episode % 100 == 0:
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, scores_mean[-1]))
-        if scores_mean[-1]>=12.0 and max_score < scores_mean[-1]:
-            max_score = scores_mean[-1]
+            print('\rEpisode {}\tAverage Score: {:.2f}\tMin Score: {:.2f}\tMax Score: {:.2f}'.format(i_episode, track['score_mean'][-1], track['score_min'][-1], track['score_max'][-1]))
+        if track['score_mean'][-1]>=12.0 and max_score < track['score_mean'][-1]:
+            max_score = track['score_mean'][-1]
             torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
-
-    return scores
+        
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        
+    if not do_visualization: monitor.draw(track)
+    plt.ioff()
+    plt.show()
+    return track['score']
 
 if __name__ == "__main__":
-    plt.ion()
 
-    env = UnityEnvironment(file_name="Banana.x86_64", seed=0)
-    agent = Agent(state_size=37, action_size=4, seed=0)
+    env = UnityEnvironment(file_name="Banana.x86_64", seed=7523)
+    agent = Agent(state_size=37, action_size=4, seed=7919)
     agent.qnetwork_local.load_state_dict(torch.load('checkpoint.pth'))
-    scores = dqn(env, agent, n_episodes=4000, eps_start=0.04, eps_end=0.01, eps_decay=0.995)
+    score = dqn(env, agent, n_episodes=10000, eps_start=1.0, eps_end=0.01, eps_decay=0.998, window_size=100, do_visualization=True)
 
-    env.close()
+    nv.close()
